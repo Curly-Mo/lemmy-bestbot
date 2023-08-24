ddd49f7e7338ab29a5189aecfdca2eae6aa12c15
Colin Fahy <colinfahy@spotify.com>
2023-08-24 02:45:15 -0400

    checkpoint3

--- src/bot.ts
+++ src/bot.ts
@@ -19,14 +19,16 @@ export const youtube = new Youtube();
 // };
 
 const communityToPlaylistId: Map<string, string> = new Map(Object.entries({
-  "importantvideos": "PLHwBlZp_DJfnU1KAem7IYHDNXjVmIxAlv",
+  "importantvideos": "PLHwBlZp_DJfmuZceDJJsIVbal9JO_hteM",
   "bideos": "PLHwBlZp_DJfl2vj6hjEmbmT7LVk9YD0bX",
   "sketchy": "PLHwBlZp_DJfkHuKW-XFJT4XEafTIRnRVQ",
 }));
 
 export class BestBot extends lemmybot.LemmyBot {
   public static youtube: Youtube = new Youtube();
-  public static postSemaphore: number = 0;
+  public static errorReprocessMinutes: number = 1;
+  public static lowScoreReprocessMinutes: number = 10;
+  // public static postSemaphore: number = 0;
   // readonly youtube: Youtube;
   constructor(botOptions: lemmybot.BotOptions) {
     super(botOptions);
@@ -48,11 +50,11 @@ export class BestBot extends lemmybot.LemmyBot {
       console.log("adding to: ", playlistId);
       this.youtube.addVideoToPlaylistNoDupes(playlistId, postView.post.url, callback);
     } else {
-      console.warn("Not adding post with invalid youtube url: ", postView);
+      console.warn("Not adding post with invalid youtube url: ", postView.post.name);
       callback(null, null);
     }
   }
-  
+
 }
 
 export const bestbot: BestBot = new BestBot({
@@ -63,7 +65,7 @@ export const bestbot: BestBot = new BestBot({
     password: LEMMY_PASSWORD,
   },
   // dbFile: 'db.sqlite3',
-  federation: 'local',
+  // federation: 'local',
   // federation: {
   //   allowList: [
   //     {
@@ -72,13 +74,21 @@ export const bestbot: BestBot = new BestBot({
   //     },
   //   ],
   // },
+  federation: {
+    allowList: [
+      {
+        instance: LEMMY_INSTANCE,
+        communities: ["bideos"],
+      },
+    ],
+  },
   connection: {
-    minutesUntilReprocess: 1,
-    secondsBetweenPolls: 120
+    // minutesUntilReprocess: 1,
+    secondsBetweenPolls: 30
   },
   handlers: {
     post: {
-      sort: 'Old',
+      sort: 'New',
       handle: ({
         postView,
         // postView: {
@@ -87,22 +97,36 @@ export const bestbot: BestBot = new BestBot({
         //   counts: { score }
         // },
         // botActions: { }
-        preventReprocess
+        reprocess
       }) => {
-        console.info("semaphore1: ", BestBot.postSemaphore);
-        if (postView.counts.score > 25) {
+        // console.info("semaphore1: ", BestBot.postSemaphore);
+        if (postView.counts.score > 20) {
+          const publishedTimestamp = new Date(postView.post.published);
+          const now = new Date();
+          const anchor = 1680000000000
+          const scheduleMillis = Math.pow(((publishedTimestamp.getTime() - anchor) / (now.getTime() - anchor)), 64)*10000;
+          console.info("now: ", now);
+          console.info("published: ", publishedTimestamp);
+          console.info("scheduleMillis: ", scheduleMillis);
+          console.info("scheduling handler for: ", postView.post.name, " in ", scheduleMillis / 1000, " seconds");
           setTimeout(() => BestBot.videoPostHandler(postView, (err, resp) => {
-            BestBot.postSemaphore--;
-            console.info("semaphore2: ", BestBot.postSemaphore);
+            // BestBot.postSemaphore--;
+            // console.info("semaphore2: ", BestBot.postSemaphore);
             if(err) {
               console.warn("errored! ", err);
+              console.info("reprocessing: ", postView.post.name, " in ", BestBot.errorReprocessMinutes, " minutes");
+              reprocess(BestBot.errorReprocessMinutes);
             } else {
-              preventReprocess();
+              // console.info("preventing reprocess: ", postView.post.name);
+              // preventReprocess();
             }
-          }), 5000 * BestBot.postSemaphore);
-          BestBot.postSemaphore++;
+          }), scheduleMillis);
+          // }), 5000 * BestBot.postSemaphore);
+          // BestBot.postSemaphore++;
         } else {
-          console.info("score too low: ", postView);
+          console.info("score too low: ", postView.post.name, "; upvotes: ", postView.counts.upvotes);
+          console.info("reprocessing: ", postView.post.name, " in ", BestBot.lowScoreReprocessMinutes, " minutes");
+          reprocess(BestBot.lowScoreReprocessMinutes);
         }
       }
     }
