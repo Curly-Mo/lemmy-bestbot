c554bbf05cdc3353249fefc118a4573a8353a53c
Colin Fahy <colinfahy@spotify.com>
2023-09-08 22:35:47 -0400

    enable both bots

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,5 @@
 import { bestbot } from './bestbot.js';
 import { sausagebot } from './sausagebot.js';
 
-// declare const global: {
-//   [x: string]: any;
-// };
-
-// global.stuff = (): void => {
-//   bestbot.start();
-// };
-
-
-// youtube.authorize(() => bestbot.start());
-// bestbot.start(); 
+bestbot.start(); 
 sausagebot.start(); 
