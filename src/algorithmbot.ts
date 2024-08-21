784159386024fcbb1c82c18ad04c7db393fecddd
Colin Fahy <colinfahy@spotify.com>
2024-08-20 21:31:50 -0400

    add new community

--- src/algorithmbot.ts
+++ src/algorithmbot.ts
@@ -15,13 +15,14 @@ const CommunityToPlaylistId: Map<string, string> = new Map(Object.entries({
   "importantvideos": "PLHwBlZp_DJfmuZceDJJsIVbal9JO_hteM",
   "bideos": "PLHwBlZp_DJfl2vj6hjEmbmT7LVk9YD0bX",
   "sketchy": "PLHwBlZp_DJfkHuKW-XFJT4XEafTIRnRVQ",
-  // "worksofart": "PLHwBlZp_DJfkC1gPkrRPtxGxeeYJAOCwh",
+  "worksofart": "PLHwBlZp_DJfkC1gPkrRPtxGxeeYJAOCwh",
   "aminals": "PLHwBlZp_DJfnndZCZdfyAwA9lNpxGO4v0",
-  // "musicbideos": "PLHwBlZp_DJfnfMU7n5IT-8CfPRbJAyb-L",
+  "musicbideos": "PLHwBlZp_DJfnfMU7n5IT-8CfPRbJAyb-L",
   "norm": "PLHwBlZp_DJfkB6Tm-CKNe_IplzaMIXtYE",
   "standup": "PLHwBlZp_DJflhO0ifYF1mXEAAal58-E0o",
   "mealtimevideos": "PLHwBlZp_DJfn_dCiWOFHIVY5SHf3iBF_D",
   "memestalgia": "PLHwBlZp_DJfldRJ0DvLyeYzZVAUqT0ATF",
+  "panelshow": "PLHwBlZp_DJflkEDf33bF7g_pxZD2JWEOF",
 }));
 
 export class AlgorithmBot extends lemmybot.LemmyBot {
